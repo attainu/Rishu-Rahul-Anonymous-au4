d5a12949ae6a04f2551f5e6fdc0ea006e8bd962a
rishu <54232477+Rishu-raj-vst-au4@users.noreply.github.com>
2020-05-06 22:21:34 +0530

    index.js Updated

--- amazon prime/server/routes/index.js
+++ amazon prime/server/routes/index.js
@@ -4,7 +4,24 @@ var router = express.Router();
 /* GET home page. */
 var userRouter = require('./users');
 var movieRouter = require('./movie');
+var episodeRouter = require('./episode');
+var showRouter = require('./show');
+var seasonRouter = require('./season');
+var genreRouter = require('./genre');
+var directorRouter = require('./director');
+var actorRouter = require('./actor');
+ 
+
 app.use('/user', userRouter);
 app.use('/movie', movieRouter);
+app.use('/episodes', episodeRouter);
+app.use('/show', showRouter);
+app.use('/season', seasonRouter);
+app.use('/movie', genreRouter);
+app.use('/director',directorRouter);
+app.use('/actor',actorRouter);
+
+
+
 
 module.exports = router;
